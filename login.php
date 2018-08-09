713e95a84dd3ee0436ff202e816afe74f0740d11
Michael Luthor <michaelluthor@163.com>
2018-08-09 21:47:04 +0800

    add login & register view

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Goku</title>
+    <link href="/assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
+    <script src="/assets/lib/bootstrap/js/bootstrap.min.js"></script>
+  </head>
+  <body style="padding-top:50px;">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-4">
+        </div>
+        <div class="col-md-4">
+         <br><br>
+         <h2>Welcome To Goku</h2>
+         <br><br><br><br>
+        <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <button type="submit" class="btn btn-default">Register</button>
+  <button type="submit" class="btn btn-default pull-right">Login</button>
+</form>
+
+
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
